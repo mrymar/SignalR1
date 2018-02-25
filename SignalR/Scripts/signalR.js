13c68ee65ab79636703bf0dcae9e3fa938676b7c
mrymar <36817311+mrymar@users.noreply.github.com>
2018-02-25 14:36:26 +0000

    Initial commit

---
+++ SignalR/Scripts/signalR.js
@@ -0,0 +1,12 @@
+﻿
+$.connection.hub.start()
+	.done(function() {
+		$.connection.myHub.server.announce("helllo");
+		console.log("worked.");
+	})
+	.fail(function() { });
+
+$.connection.myHub.client.announce = function(message) {
+	console.log(message);
+	$('#myMessage').append(message);
+}
